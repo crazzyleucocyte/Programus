78a1e02b0c2549e4684bc754a86a59a014edbe19
crazzyleucocyte <alstjr7696@naver.com>
2024-05-23 17:47:10 +0900

    [level 0] Title: 문자열 돌리기, Time: 132.49 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+     char clock[] = a.toCharArray();
+        for(char at:clock){
+        System.out.println(at);
+        }
+    }
+    
+}
