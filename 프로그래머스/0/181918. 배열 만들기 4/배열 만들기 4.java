fbc25df4e80539d9b2395f088cbbe3836dbffaa7
crazzyleucocyte <alstjr7696@naver.com>
2024-06-27 20:52:14 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.88 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,27 +1,18 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
-    public int[] solution(int[] arr) {
-        List <Integer>stk = new ArrayList();
-        
-        for(int i =0;i<arr.length;i++ ){
-            
-            if(stk.size()==0){
+    public Stack solution(int[] arr) {
+        Stack<Integer> stk = new Stack();
+        for(int i = 0;i<arr.length;i++){
+            if(stk.isEmpty()){
                 stk.add(arr[i]);
-                
-            }else if(stk.get(stk.size()-1)<arr[i]){
-                stk.add(arr[i]);
-                
-            }else if(stk.get(stk.size()-1)>=arr[i]){
-                stk.remove(stk.size()-1);
+            }else if(stk.peek()>=arr[i]){
+                stk.pop();
                 i--;
+            }else if(stk.peek()<arr[i]){
+                stk.push(arr[i]);
             }
-            
         }
-            int answer[]=new int[stk.size()];
-            for(int i=0;i<stk.size();i++){
-                answer[i]=(int)stk.get(i);
-            }
-        return answer;
+       
+        return stk;
     }
 }
