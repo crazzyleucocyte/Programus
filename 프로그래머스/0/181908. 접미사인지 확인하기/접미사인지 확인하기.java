d0c7de06e5d5c540a6c7931091d9817a1e601a53
crazzyleucocyte <alstjr7696@naver.com>
2024-06-30 13:43:26 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,12 +1,7 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
 
-        for(int i =0;i<my_string.length();i++){
-            
-            if(my_string.substring(i).equals(is_suffix)){
-                return 1;
-            }
-        }
-        return 0;
+        
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
