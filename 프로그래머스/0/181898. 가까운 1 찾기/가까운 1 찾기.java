e109392553b094736a26109f385d736cec77c441
crazzyleucocyte <alstjr7696@naver.com>
2024-06-30 23:27:55 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for ( int i = idx ; i < arr.length ; i++){
+            if ( arr[i] == 1 ) return i;
+        }
+        return -1;
+    }
+}
