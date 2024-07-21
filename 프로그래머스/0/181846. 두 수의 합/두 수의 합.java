f96df6f18a14a46f3daf92411c109b792dfc12be
crazzyleucocyte <alstjr7696@naver.com>
2024-07-21 18:43:29 +0900

    [level 0] Title: 두 수의 합, Time: 234.13 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public String solution(String a, String b) {
+        StringBuilder answer=new StringBuilder();
+
+       int length = (a.length()>b.length())? a.length():b.length();
+        int temp =0;
+        String stra[]=a.split("");
+        String strb[]=b.split("");
+            System.out.println(length);
+        int cha=0;
+        int chb=0;
+        int temp2=0;
+        int i=stra.length-1;
+        int j=strb.length-1;
+        for(int idx =length-1;idx>=0;idx--,i--,j--){
+            cha=(i<0)?0:Integer.parseInt(stra[i]);
+            chb=(j<0)?0:Integer.parseInt(strb[j]);
+            
+            temp = cha+chb;
+            
+            if(idx!=0)
+            answer.insert(0, String.valueOf((temp2+temp)%10));
+            else 
+            answer.insert(0, String.valueOf(temp2+temp));
+            
+            temp2=((temp+temp2)/10);
+
+        }
+        return answer.toString();
+    }
+}
