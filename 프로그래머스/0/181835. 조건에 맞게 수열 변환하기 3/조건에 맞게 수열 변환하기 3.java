3dde88a2ee3c6473925d9e34a9f7ffc440fee99c
crazzyleucocyte <alstjr7696@naver.com>
2024-07-21 23:06:12 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 9.74 ms, Memory: 170 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+        for(int i =0;i<arr.length;i++){
+            answer[i]=(k%2==0) ? arr[i]+k: arr[i]*k;
+        }
+        return answer;
+    }
+}
